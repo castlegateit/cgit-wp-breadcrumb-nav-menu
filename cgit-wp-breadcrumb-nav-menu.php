e252fcf6b9d234515f8c8f973462a6c1f0a03da7
John Hughes <john@castlegateit.co.uk>
2017-07-21 09:12:27 +0100

    Switch to parent plugin action to load plugin

--- cgit-wp-breadcrumb-nav-menu.php
+++ cgit-wp-breadcrumb-nav-menu.php
@@ -5,29 +5,20 @@
 Plugin Name: Castlegate IT WP Breadcrumb Nav Menu
 Plugin URI: http://github.com/castlegateit/cgit-wp-breadcrumb-nav-menu
 Description: Simple breadcrumb navigation for WordPress using WordPress Nav Menus.
-Version: 3.0
+Version: 3.1
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 License: MIT
 
 */
 
-/**
- * Load plugin
- */
-add_action('plugins_loaded', function () {
-    if (!class_exists('Cgit\Breadcrumb')) {
-        add_action('admin_notices', function () {
-            ?>
-            <div class="error">
-                <p><strong>Error:</strong> the <a href="http://github.com/castlegateit/cgit-wp-breadcrumb-nav-menu">Breadcrumb Nav Menu plugin</a> requires the <a href="http://github.com/castlegateit/cgit-wp-breadcrumb">Breadcrumb plugin</a>.</p>
-            </div>
-            <?php
-        });
+if (!defined('ABSPATH')) {
+    wp_die('Access denied');
+}
 
-        return;
-    }
-
+add_action('cgit_breadcrumb_loaded', function () {
     require_once __DIR__ . '/classes/autoload.php';
     require_once __DIR__ . '/functions.php';
-}, 20);
+
+    do_action('cgit_breadcrumb_nav_menu_loaded');
+});
